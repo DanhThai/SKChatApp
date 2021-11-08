8018f1d9caea3abad1607b399b0c213dd8a68100
DinhSan <ledinhsan2001@gmail.com>
2021-11-08 15:34:48 +0700

    Model-connect

---
+++ Source_code/SK_Server/ProcessServer/ProcessServer/DB_Connect.Designer.cs
@@ -0,0 +1,10 @@
+﻿// T4 code generation is enabled for model 'D:\DoAn\PBL4 HDH và Mang May Tinh\SKChatApp\Source_code\SK_Server\ProcessServer\ProcessServer\DB_Connect.edmx'. 
+// To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
+// property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
+// is open in the designer.
+
+// If no context and entity classes have been generated, it may be because you created an empty model but
+// have not yet chosen which version of Entity Framework to use. To generate a context class and entity
+// classes for your model, open the model in the designer, right-click on the designer surface, and
+// select 'Update Model from Database...', 'Generate Database from Model...', or 'Add Code Generation
+// Item...'.
